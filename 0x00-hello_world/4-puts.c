2615ce956c21b2167862ed35afb01e1f7123b258
Bismark Agbemafle <agbemaflebismark@gmail.com>
2023-06-15 22:58:18 -0700

    C Processor

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a phrase with proper grammar and returns 0
+ * Return: 0 (Exit success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
